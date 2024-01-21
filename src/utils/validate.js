4c24bd0f6cf7c3179f8e9d37f06df211d7853f4a
khushaalsareen <khushaalsareen@gmail.com>
2024-01-21 22:00:18 +0530

    password validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,6 +1,6 @@
 export const checkValidData = (email,password,myName)=>{
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
-    const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)
+    const isPasswordValid = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password)
     const isNameValid = /^[\p{L} ,.'-]+$/u.test(myName)
 
     if(myName!=null && !isNameValid) return "Name is not valid"
