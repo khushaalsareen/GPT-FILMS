cb20c7d90f46a32d43db48405818593016c22655
khushaalsareen <khushaalsareen@gmail.com>
2024-01-21 21:47:47 +0530

    Name Validations

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,7 @@
 export const checkValidData = (email,password,myName)=>{
     const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)
-    const isNameValid = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(myName)
+    const isNameValid = /^[\p{L} ,.'-]+$/u.test(myName)
 
     if(myName!=null && !isNameValid) return "Name is not valid"
 
