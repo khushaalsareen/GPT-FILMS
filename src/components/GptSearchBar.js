456b9e4597787c1aaf6f31ddcf5d467f1e209762
khushaalsareen <khushaalsareen@gmail.com>
2024-07-28 20:14:20 +0530

    changed to gemini api

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,62 +1,60 @@
-import React, { useRef } from 'react'
-import lang from '../utils/languageConstants'
-import { useDispatch, useSelector } from 'react-redux'
-import openai from '../utils/openai'
-import { API_OPTIONS } from '../utils/constants'
-import { addGptMovieResult, clearGptMovieResult, pressedSearch, undoSearch } from '../utils/gptSlice'
+import React, { useRef } from 'react';
+import lang from '../utils/languageConstants';
+import { useDispatch, useSelector } from 'react-redux';
+import { API_OPTIONS } from '../utils/constants';
+import { addGptMovieResult, clearGptMovieResult, pressedSearch, undoSearch } from '../utils/gptSlice';
+import run from './gemini';
+
 
 const GptSearchBar = () => {
-  const langKey = useSelector(store=>store.config.lang)
+  const langKey = useSelector(store => store.config.lang);
   const searchText = useRef(null);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  // search movie in TMDB
-  const searchMovieTMDB = async(movie)=>{
-    const data = await fetch('https://api.themoviedb.org/3/search/movie?query='+movie+'&include_adult=false&language=en-US&page=1',API_OPTIONS)
+  // Search movie in TMDB
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch('https://api.themoviedb.org/3/search/movie?query=' + movie + '&include_adult=false&language=en-US&page=1', API_OPTIONS);
     const json = await data.json();
-    return json.results
-  }
+    return json.results;
+  };
 
-  const handleGptSearchClick = async()=>{
-    
+  const handleGptSearchClick = async () => {
     // Make an API call to GPT API and get Movie Results
-    if(searchText.current.value ===""){
-      dispatch(clearGptMovieResult())
-      dispatch(undoSearch())
-      return null
+    if (searchText.current.value === "") {
+      dispatch(clearGptMovieResult());
+      dispatch(undoSearch());
+      return null;
     }
-    dispatch(clearGptMovieResult())
+    dispatch(clearGptMovieResult());
     dispatch(pressedSearch());
-   
-    const gptQuery = "Act as a Movie Recommendation system and suggest some movies for thr query: " + searchText.current.value + ". only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya"
 
-    const gptResults = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: gptQuery }],
-      model: 'gpt-3.5-turbo',
-    });
+    const gptQuery = "Act as a Movie Recommendation system and suggest some movies for the query: " + searchText.current.value + ". Only give me names of 5 movies, comma separated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
 
-   
-    const gptMovies = gptResults.choices?.[0]?.message?.content.split(",") // gives array of movies
+    try {
+      const gptResults = await run(gptQuery);  // Use the run function to get results from Gemini API
+      const gptMovies = gptResults.split(","); // Split the response to get an array of movies
 
-    // For each movie I will search TMDB API
-    const promiseArray = gptMovies.map(movie=>searchMovieTMDB(movie));
-    // [Promise,Promise,Promise,Promise,Promise,]
+      // For each movie, search TMDB API
+      const promiseArray = gptMovies.map(movie => searchMovieTMDB(movie));
+      // [Promise,Promise,Promise,Promise,Promise,]
 
-    const tmdbResults = await Promise.all(promiseArray);
+      const tmdbResults = await Promise.all(promiseArray);
 
-    dispatch(addGptMovieResult({movieNames:gptMovies, movieResults: tmdbResults}));
-  }
+      dispatch(addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults }));
+    } catch (error) {
+      console.error('Error fetching movie recommendations:', error);
+      dispatch(undoSearch());
+    }
+  };
 
   return (
     <div className='pt-[15%] md:pt-[15%] lg:pt-[10%] flex justify-center'>
-        
-      <form className='w-[90%] bg-black md:w-1/2 grid grid-cols-12 md:p-0 ' onSubmit={(e)=>e.preventDefault()}>
-            
-            <input ref={searchText} type="text" className='p-2 m-3 col-span-6 md:p-4 md:m-4 md:col-span-8 lg:col-span-9 md:w-[95%] w-60 font-bold' placeholder={lang[langKey].gptSearchPlaceholder}/>
-            <button onClick={ handleGptSearchClick} className='ml-20  md:px-4 md:py-2 px-3 md:w-[85%] w-20  m-3 md:m-4 bg-red-600 text-white rounded-md  md:col-span-4 col lg:col-span-3'>{lang[langKey].search}</button>
-        </form>
+      <form className='w-[90%] bg-black md:w-1/2 grid grid-cols-12 md:p-0 ' onSubmit={(e) => e.preventDefault()}>
+        <input ref={searchText} type="text" className='p-2 m-3 col-span-6 md:p-4 md:m-4 md:col-span-8 lg:col-span-9 md:w-[95%] w-60 font-bold' placeholder={lang[langKey].gptSearchPlaceholder} />
+        <button onClick={handleGptSearchClick} className='ml-20  md:px-4 md:py-2 px-3 md:w-[85%] w-20  m-3 md:m-4 bg-red-600 text-white rounded-md  md:col-span-4 col lg:col-span-3'>{lang[langKey].search}</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default GptSearchBar
+export default GptSearchBar;
---
+++ src/components/gemini.js
@@ -0,0 +1,28 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const apiKey = process.env.REACT_APP_OPENAI_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  return result.response.text();  // Ensure this returns a plain text response
+}
+
+export default run;
